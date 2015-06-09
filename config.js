7f293a54055510dea8adff9c42834f7fe567db87
phxrefugee <patrickm@gtsservices.com>
2015-06-09 09:12:36 -0700

    added comments to config.js

--- config.js
+++ config.js
@@ -3,19 +3,24 @@
  */
 
 var config = {
+    // mongoDB connection
     //mongoConnection: 'mongodb://flights:flights@ds031832.mongolab.com:31832/flights',
     mongoConnection: 'mongodb://standup:standup@ds034348.mongolab.com:34348/demos',
+    // update promise connection
     accessKey: '33e97066ad9bc9cb1a15ed86316da370913427a0',
     updatePromiseHost: 'test.updatepromise.com',
     updatePromisePort: '443',
     updatePromisePath: '/api/promise_api_glass.py',
+    // sql connection
     sqlUser: 'sa',
     sqlPassword: 'UN0tN0This!',
     sqlServer: 'localhost',
     sqlDatabase: 'MessagingQueue',
+    // geoName connection
     geoNamesHost: 'api.geonames.org',
     geoNamesPort: '80',
     geoNamesUser: 'patrickm',
+    // polling frequency
     pollingInterval: 10000 // milliseconds between calls
 };
 
